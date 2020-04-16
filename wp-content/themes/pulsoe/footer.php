281721128eb0c2e6cc4c60288ce8f6c5d887931e
Lucas <lucasg.ruizd@gmail.com>
2020-04-15 17:57:44 -0700

    Preparados archivos para el nuevo tema

---
+++ wp-content/themes/pulsoe/footer.php
@@ -0,0 +1,9 @@
+    </div> <!-- #main-container -->
+  <div class="footer-container">
+    <footer class="wrapper">
+      <p class="footer-credits">© 2019 My Imaginary Website</p>
+    </footer>
+  </div>
+    <?php wp_footer();?>
+  </body>
+</html>
---
+++ wp-content/themes/pulsoe/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html ng-app="pulsoe" lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <link rel="icon" href="<?php echo get_theme_file_uri(); ?>/assets/images/logo1.ico">
+        <!-- PageInfo -->
+        <meta name="description"
+            content="Bolsa de trabajo y recursos humanos en Hermosillo, Sonora, con
+            30 años de experiencia a su servicio nos ponemos en sus manos para brindarle la mejor opción de
+            búsqueda de personal en la región noroeste de la república">
+
+        <meta name="author" content="Sonnencode">
+        <meta name="keywords" content="recursos humanos,Hermosillo,Sonora,pulso,empresarial,sonnencode,outsourcing">
+        <meta name="robots" content="index,nofollow">
+        <meta name="googlebot" content="noarchive">
+
+        <!-- Open Graph data -->
+        <meta property="og:title" content=""/>
+        <meta property="og:type" content=""/>
+        <meta property="og:url" content=""/>
+        <meta property="og:image" content=""/>
+        <meta property="og:description" content=""/>
+
+        <title ng-bind-template="Pulso Empresarial">Pulso Empresarial</title>
+
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" media="all" />
+
+        <script src="angular-app/main/app.js"></script>
+        
+        <?php wp_head();?>
+    </head>
+    <body>
+        <div id="main-container" autoscroll="true">
